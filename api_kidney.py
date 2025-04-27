f62f4eb4692895696e51ff291b6c56ce0e1ae188
BLaZe-31 <abhinavsrijan1234@gmail.com>
2025-04-27 18:13:58 +0530

    inital commit

--- api_kidney.py
+++ api_kidney.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import numpy as np
+import pandas as pd
 import joblib
 import asyncio
 import uvicorn
 import os
 from fastapi.middleware.cors import CORSMiddleware
 
-model = joblib.load(os.path.join("models", "kidney_disease_model.pkl"))
-
-
+model_path = os.path.join("models", "kidney_disease_model.pkl")
+pipeline, top_features = joblib.load(model_path)
 
 app = FastAPI()
 
@@ -17,9 +17,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],  
+    allow_headers=["*"],  
 )
 
 class KidneyDiseaseInput(BaseModel):
@@ -32,30 +31,32 @@ class KidneyDiseaseInput(BaseModel):
     SerumElectrolytesSodium: float
     SystolicBP: float
     HemoglobinLevels: float
-    
 
 @app.api_route("/ping", methods=["GET", "HEAD"])
 async def ping():
     await asyncio.sleep(0.1)
     return {"message": "Server is running"}
 
+
 @app.post("/predict")
 def predict(data: KidneyDiseaseInput):
-    input_data = np.array([[ 
-    data.SerumCreatinine,
-    data.GFR,
-    data.ProteinInUrine,
-    data.FastingBloodSugar,
-    data.BUNLevels,
-    data.HbA1c,
-    data.SerumElectrolytesSodium,
-    data.SystolicBP,
-    data.HemoglobinLevels
-]])
 
+    input_data = [
+        data.SerumCreatinine,
+        data.GFR,
+        data.ProteinInUrine,
+        data.FastingBloodSugar,
+        data.BUNLevels,
+        data.HbA1c,
+        data.SerumElectrolytesSodium,
+        data.SystolicBP,
+        data.HemoglobinLevels
+    ]
 
-    prediction = model.predict(input_data)[0]
-    confidence = model.predict_proba(input_data)[0][prediction] * 100
+    input_df = pd.DataFrame([input_data], columns=top_features)
+
+    prediction = pipeline.predict(input_df)[0]
+    confidence = pipeline.predict_proba(input_df)[0][prediction] * 100
 
     if prediction == 1:
         return {
@@ -71,3 +72,4 @@ def predict(data: KidneyDiseaseInput):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
+
